6ec6f2fd6d0189b3bdc9be05cbdb3534d4ee4e61
ozzyfant <ozzy2345de@gmail.com>
2013-05-26 14:17:00 +0200

    Credit to Nox

--- lib/Autoloader.class.php
+++ lib/Autoloader.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @author    Janek Ostendorf (ozzy) <ozzy2345de@gmail.com>
+ * @author    Patrick Kleinschmidt (NoxNebula) <noxifoxi@gmail.com>. Rewritten by Janek Ostendorf (ozzy) <ozzy2345de@gmail.com>
  * @copyright Copyright (c) 2013 SilexLab
  * @license   http://opensource.org/licenses/gpl-3.0.html GNU General Public License, version 3
  */
