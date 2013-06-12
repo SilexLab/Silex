6842989f4e917c3b9473300b9ccf9717c38e6879
Patrick <noxifoxi@gmail.com>
2013-06-12 15:29:52 +0200

    message parser placeholder

---
+++ lib/message/MessageParser.class.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author    Patrick Kleinschmidt (NoxNebula) <noxifoxi@gmail.com>
+ * @copyright Copyright (c) 2013 SilexLab
+ * @license   http://opensource.org/licenses/gpl-3.0.html GNU General Public License, version 3
+ */
+
+class MessageParser {
+	// Ah yea... message parser...
+}
