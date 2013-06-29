ea2cd92a648ce64884543bf8f34407b203e47ffc
ozzyfant <ozzy2345de@gmail.com>
2013-06-29 23:54:21 +0200

    It does work now

--- test_setup.sh
+++ test_setup.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 # Setup MySQL
+echo "Creating database ..."
 mysql -e 'create database silex_db;'
+echo "Populating database ..."
 mysql silex_db < docs/silex.sql
 
 # Setup config
+echo "Generating config file ..."
 touch lib/config.inc.php
 echo "<?php"                               > lib/config.inc.php
 echo "return ["                            >> lib/config.inc.php
@@ -14,4 +17,4 @@ echo "'database.name'     => 'silex_db',"  >> lib/config.inc.php
 echo "'database.port'     => 3306,"        >> lib/config.inc.php
 echo "'database.wrapper'  => 'mysql'"      >> lib/config.inc.php
 echo "];"                                  >> lib/config.inc.php
-
+echo "Done."
