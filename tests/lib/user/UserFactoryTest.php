23522c3964023b16df568e121a4977bc98ef513e
ozzyfant <ozzy2345de@gmail.com>
2013-07-02 15:42:06 +0200

    Test user factory

---
+++ tests/lib/user/UserFactoryTest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @author    Janek Ostendorf (ozzy) <ozzy2345de@gmail.com>
+ * @copyright Copyright (c) 2013 SilexLab
+ * @license   http://opensource.org/licenses/gpl-3.0.html GNU General Public License, version 3
+ */
+ 
+class UserFactoryTest extends PHPUnit_Framework_TestCase {
+	public function testProperUserFetching() {
+		$userByID = $userByName = null;
+		$guestUser = UserFactory::getGuest();
+		try {
+			$userByName = UserFactory::getUserByName('admin');
+			$userByID = UserFactory::getUserByID(1);
+		} catch(UserNotFoundException $e) {
+			$this->fail('UserNotFoundException thrown while the user exists');
+		}
+
+		$this->assertInstanceOf('GuestUser', $guestUser, 'Guest is no instance of GuestUser.');
+		$this->assertInstanceOf('User', $userByName, 'UserByName is no instance of User.');
+		$this->assertInstanceOf('User', $userByID, 'UserByID is no instance of User.');
+
+		$this->assertEquals($userByID, $userByName, 'Users are not equal, although they must.');
+	}
+
+	public function testFactoryThrowsExceptionIfNotExistsByName() {
+		try {
+			$userByName = UserFactory::getUserByName('IDoNotExist');
+		} catch(UserNotFoundException $e) {
+			return;
+		}
+		$this->fail('UserNotFoundException hasn\'t been thrown');
+	}
+
+	public function testFactoryThrowsExceptionIfNotExistsByID() {
+		try {
+			$userByID = UserFactory::getUserByID(-42);
+		} catch(UserNotFoundException $e) {
+			return;
+		}
+		$this->fail('UserNotFoundException hasn\'t been thrown');
+	}
+}
