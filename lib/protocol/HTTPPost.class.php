e943bf2ad957da62790b6bde4ee31df4f54b0396
Patrick Kleinschmidt <patrick.kl@live.de>
2014-03-17 21:26:33 +0100

    updated class head

--- lib/protocol/HTTPPost.class.php
+++ lib/protocol/HTTPPost.class.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @author    SilexLab <labs@silexlab.org>
- * @copyright Copyright (c) 2013 SilexLab
+ * @copyright Copyright (c) 2014 SilexLab
  * @license   http://opensource.org/licenses/gpl-3.0.html GNU General Public License, version 3
  */
  
