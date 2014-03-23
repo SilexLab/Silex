4d0e8e07c5534c5f2e6a51f171ba2a1fd77bf122
Patrick Kleinschmidt <patrick.kl@live.de>
2014-03-23 21:45:50 +0100

    forgot database...

--- docs/silex.sql
+++ docs/silex.sql
@@ -1,12 +1,6 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
-
 DROP TABLE IF EXISTS `config`;
 CREATE TABLE IF NOT EXISTS `config` (
   `option` varchar(255) NOT NULL,
@@ -29,6 +23,21 @@ INSERT INTO `config` (`option`, `value`, `type`, `package`) VALUES
 ('url.base', '/', 'string(255)', 1),
 ('url.format', '1', 'int(1)', 1);
 
+DROP TABLE IF EXISTS `navigation`;
+CREATE TABLE IF NOT EXISTS `navigation` (
+  `id` tinyint(4) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `target` varchar(255) NOT NULL,
+  `position` tinyint(4) NOT NULL,
+  `permission` tinytext NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `Target` (`target`),
+  UNIQUE KEY `position` (`position`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+INSERT INTO `navigation` (`id`, `title`, `target`, `position`, `permission`) VALUES
+(1, 'page.home', 'home', 1, '');
+
 DROP TABLE IF EXISTS `session`;
 CREATE TABLE IF NOT EXISTS `session` (
   `id` varchar(32) NOT NULL,
@@ -42,6 +51,9 @@ CREATE TABLE IF NOT EXISTS `session` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+INSERT INTO `session` (`id`, `session_value`, `user_id`, `username`, `ip_address`, `user_agent`, `last_activity`, `token`) VALUES
+('igua9pdcat6fo8ad9ob8m6ofu0tlgtg4', '', 0, '', '', '', 1395607276, '');
+
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(8) NOT NULL AUTO_INCREMENT,
@@ -53,7 +65,3 @@ CREATE TABLE IF NOT EXISTS `user` (
 
 INSERT INTO `user` (`id`, `name`, `password`, `mail`) VALUES
 (1, 'admin', '', 'admin@silexlab.org');
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
