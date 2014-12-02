2976a866c600d4811145c1e78da6e369b7af2dd0
Patrick Kleinschmidt <patrick.kl@live.de>
2014-12-02 17:21:52 +0100

    Fixed Version utility class - optimized code - changed return to:   - version1 < version2 -> -1   - version1 = version2 ->  0   - version1 > version2 ->  1 - added missing operators - added rc release state

--- lib/util/Version.php
+++ lib/util/Version.php
@@ -14,12 +14,22 @@ namespace silex\util;
 class Version
 {
 	/**
+	 * RegeX Replace A-Z
+	 */
+	const RX_R_AZ = '/\.?([a-z]+)\.?/i';
+
+	/**
+	 * String Replace To Period
+	 */
+	const S_R_TP = ['_', '-', '+'];
+
+	/**
 	 * Compares two version with each other
 	 *
 	 * Return values:
-	 *  0 - $version1 < $version2
+	 * -1 - $version1 < $version2
+	 *  0 - $version1 = $version2
 	 *  1 - $version1 > $version2
-	 * -1 - $version1 == $version2
 	 *
 	 * @param string $version1
 	 * @param string $version2
@@ -28,37 +38,78 @@ class Version
 	 */
 	public static function compare($version1, $version2, $operator = '')
 	{
+		// If operator is given, handle it
 		if ($operator)
 			return self::handleOperator($version1, $version2, $operator);
 
-		$state1 = preg_replace('/^[0-9]+(\.[0-9]+)+-(.*)/', '$2', $version1);
-		$state2 = preg_replace('/^[0-9]+(\.[0-9]+)+-(.*)/', '$2', $version2);
+		// Explode version1 on .
+		$v1 = explode(
+			'.',
+			// Pre- and append . to not numeric stuff
+			preg_replace(
+				self::RX_R_AZ,
+				'.$1.',
+				// Replace strings in S_R_TP with .
+				str_replace(self::S_R_TP, '.', $version1)
+			)
+		);
 
-		$v1 = explode('.', preg_replace('/^([0-9]+(\.[0-9]+)+)/', '$1', $version1));
-		$v2 = explode('.', preg_replace('/^([0-9]+(\.[0-9]+)+)/', '$1', $version2));
+		// Same as above for version2
+		$v2 = explode(
+			'.',
+			preg_replace(
+				self::RX_R_AZ,
+				'.$1.',
+				str_replace(self::S_R_TP, '.', $version2)
+			)
+		);
 
-		for ($i = 0; $i < count($v1); $i++) {
+		// Check
+		for ($i = 0; $i < max(count($v1), count($v2)); $i++) {
+			// If the provided version string is shorter than the other fill missing entries with zeros
+			if (!isset($v1[$i]))
+				$v1[$i] = 0;
 			if (!isset($v2[$i]))
 				$v2[$i] = 0;
 
-			if ($v1[$i] == $v2[$i])
+			// Continue if this part is equal
+			if (self::convert($v1[$i]) == self::convert($v2[$i]))
 				continue;
 
-			return (int)($v1[$i] > $v2[$i]);
+			// Version is different
+			return (self::convert($v1[$i]) > self::convert($v2[$i])) ? 1 : -1;
 		}
 
-		if ($state1 != $version1 && $state2 != $version2 && $state1 != $state2) {
-			$p1 = self::getHeight($state1);
-			$p2 = self::getHeight($state2);
+		// Version is equal
+		return 0;
+	}
 
-			if ($p1 > $p2)
-				return (int)($p1 > $p2);
+	/**
+	 * Gets the height of the version part
+	 *
+	 * @param string $v
+	 * @return int
+	 */
+	protected static function convert($v)
+	{
+		if (is_numeric($v))
+			return $v;
 
-			if ($state1 > $state2)
-				return (int)($state1 > $state2);
+		switch ($v) {
+			case 'dev':
+				return -4;
+			case 'a':
+			case 'alpha':
+				return -3;
+			case 'b':
+			case 'beta':
+				return -2;
+			case 'pre':
+			case 'rc':
+				return -1;
+			default:
+				return 0;
 		}
-
-		return -1;
 	}
 
 	/**
@@ -73,53 +124,34 @@ class Version
 	 * @param string $operator
 	 * @return bool
 	 */
-	private static function handleOperator($version1, $version2, $operator)
+	protected static function handleOperator($version1, $version2, $operator)
 	{
 		$result = self::compare($version1, $version2);
 
 		switch ($operator) {
+			case '=':
 			case '==':
-				return $result == -1;
+			case 'eq':
+				return $result == 0;
 			case '<':
-				return $result === 0;
+			case 'lt':
+				return $result === -1;
 			case '>':
+			case 'gt':
 				return $result === 1;
 			case '<=':
+			case 'le':
 				return $result == -1 || $result === 0;
 			case '>=':
-				return $result == -1 || $result === 1;
+			case 'ge':
+				return $result === 1 || $result === 0;
 			case '!=':
-				return $result != -1;
+			case '<>':
+			case 'ne':
+				return $result != 0;
 			
 			default:
 				return null;
-		}
-	}
-
-	/**
-	 * Gets the height of the release state
-	 *
-	 * @param string &$state
-	 * @return int
-	 */
-	private static function getHeight(&$state)
-	{
-		preg_match('/([a-z]+)([0-9]*)/', strtolower($state), $match);
-		$state = (int)$match[1];
-
-		switch ($match[0]) {
-			case 'dev':
-				return -4;
-			case 'a':
-			case 'alpha':
-				return -3;
-			case 'b':
-			case 'beta':
-				return -2;
-			case 'pre':
-				return -1;
-			default:
-				return 0;
 		}
 	}
 }
